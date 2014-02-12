a16ca67e8e3bc084f9bf3bb264f9d15ff518b6b8
justinterada <jtvkws001@gmail.com>
2014-02-11 23:49:41 -0800

    [NcaaPool2:Framework] : Comment out integration code that was making jshint sad

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,11 +1,11 @@
-var fs = require('fs');
+//var fs = require('fs');
 
-var webdriver = require('selenium-webdriver');
-var assert = require('assert');
-var path = require('path');
-var chromeDriver = require('selenium-chromedriver');
-var chrome = require('selenium-webdriver/chrome');
-var chromeService = new chrome.ServiceBuilder(chromeDriver.path);
+//var webdriver = require('selenium-webdriver');
+//var assert = require('assert');
+//var path = require('path');
+//var chromeDriver = require('selenium-chromedriver');
+//var chrome = require('selenium-webdriver/chrome');
+//var chromeService = new chrome.ServiceBuilder(chromeDriver.path);
 
 suite('MyApp', function() {
 
